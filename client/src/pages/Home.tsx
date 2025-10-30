6b9794001e2a8cd4cb5a94892982bd16c247fe58
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-10-29 22:23:48 -0300

    Add files via upload

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,27 @@
-// client/src/pages/Home.tsx
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import CardsSection from "@/components/CardsSection";
+import BuziosSection from "@/components/BuziosSection";
+import SimpatiasSection from "@/components/SimpatiasSection";
+import ConsultasSection from "@/components/ConsultasSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
+
 export default function Home() {
-  return <h1>Home - Mestre Reginaldo</h1>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <CardsSection />
+      <BuziosSection />
+      <SimpatiasSection />
+      <ConsultasSection />
+      <ContactSection />
+      <Footer />
+      <WhatsAppButton />
+    </div>
+  );
 }
